39d90c7b95301fc75b6fc12568b81355f0fa6d07
XanderSnelder <snelder.xander@gmail.com>
2023-04-21 09:27:07 +0200

    added strong password tests

--- test_auth.py
+++ test_auth.py
@@ -25,10 +25,10 @@ class TestAuthService(unittest.TestCase):
         Test if the method returns a 201 status code and that the hashed password is stored in USER_DATA.
         """
 
-        response = self.client.post('/users', json={'username': 'test_user', 'password': 'test_password'})
+        response = self.client.post('/users', json={'username': 'test_user', 'password': 'Str6ng_P@ssword!'})
         self.assertEqual(response.status_code, 201)
 
-        hashed_password = hashlib.new(HASH_ALGORITHM, 'test_password'.encode('utf-8')).hexdigest()
+        hashed_password = hashlib.new(HASH_ALGORITHM, 'Str6ng_P@ssword!'.encode('utf-8')).hexdigest()
         self.assertEqual(USER_DATA['test_user']['password'], hashed_password)
 
     def test_login(self):
@@ -38,11 +38,20 @@ class TestAuthService(unittest.TestCase):
         """
 
         # First, create a user for testing login
-        self.client.post('/users', json={'username': 'test_user', 'password': 'test_password'})
-        response = self.client.post('/users/login', json={'username': 'test_user', 'password': 'test_password'})
-        
+        self.client.post('/users', json={'username': 'test_user', 'password': 'Str6ng_P@ssword!'})
+        response = self.client.post('/users/login', json={'username': 'test_user', 'password': 'Str6ng_P@ssword!'})
+
         self.assertEqual(response.status_code, 200)
         self.assertIn('access_token', json.loads(response.data))
+
+    def test_weak_password(self):
+
+        """
+        Test if the method returns a 400 status code when attempting to create a user with a weak password.
+        """
+
+        response = self.client.post('/users', json={'username': 'test_user', 'password': 'weak_password'})
+        self.assertEqual(response.status_code, 400)
 
     def test_update_password(self):
 
@@ -51,20 +60,36 @@ class TestAuthService(unittest.TestCase):
         """
 
         # First, create a user for testing login
-        response = self.client.post('/users', json={'username': 'test_user', 'password': 'test_password'})
-        response = self.client.post('/users/login', json={'username': 'test_user', 'password': 'test_password'})
+        response = self.client.post('/users', json={'username': 'test_user', 'password': 'Str6ng_P@ssword!'})
+        response = self.client.post('/users/login', json={'username': 'test_user', 'password': 'Str6ng_P@ssword!'})
         access_token = json.loads(response.data)['access_token']
 
         # Update the password
         headers = {'Authorization': f'Bearer {access_token}'}
-        response = self.client.put('/users', json={'username': 'test_user', 'old_password': 'test_password', 'new_password': 'new_password'}, headers=headers)
+        response = self.client.put('/users', json={'username': 'test_user', 'old_password': 'Str6ng_P@ssword!', 'new_password': 'new_Str6ng_P@ssword!'}, headers=headers)
         self.assertEqual(response.status_code, 200)
 
         # Test if the password has been updated
-        old_hashed_password = hashlib.new(HASH_ALGORITHM, 'test_password'.encode('utf-8')).hexdigest()
-        new_hashed_password = hashlib.new(HASH_ALGORITHM, 'new_password'.encode('utf-8')).hexdigest()
+        old_hashed_password = hashlib.new(HASH_ALGORITHM, 'Str6ng_P@ssword!'.encode('utf-8')).hexdigest()
+        new_hashed_password = hashlib.new(HASH_ALGORITHM, 'new_Str6ng_P@ssword!'.encode('utf-8')).hexdigest()
         self.assertNotEqual(USER_DATA['test_user']['password'], old_hashed_password)
         self.assertEqual(USER_DATA['test_user']['password'], new_hashed_password)
+
+    def test_update_weak_password(self):
+        
+        """
+        Test if the method returns a 400 status code when trying to update the user's password with a weak password.
+        """
+
+        # First, create a user for testing login
+        response = self.client.post('/users', json={'username': 'test_user', 'password': 'Str6ng_P@ssword!'})
+        response = self.client.post('/users/login', json={'username': 'test_user', 'password': 'Str6ng_P@ssword!'})
+        access_token = json.loads(response.data)['access_token']
+
+        # Attempt to update the password with a weak password
+        headers = {'Authorization': f'Bearer {access_token}'}
+        response = self.client.put('/users', json={'username': 'test_user', 'old_password': 'Str6ng_P@ssword!', 'new_password': 'weakpw'}, headers=headers)
+        self.assertEqual(response.status_code, 400)
 
     def test_require_auth(self):
 
@@ -74,22 +99,22 @@ class TestAuthService(unittest.TestCase):
         """
 
         # Create a user and log in
-        self.client.post('/users', json={'username': 'test_user', 'password': 'test_password'})
-        login_response = self.client.post('/users/login', json={'username': 'test_user', 'password': 'test_password'})
+        self.client.post('/users', json={'username': 'test_user', 'password': 'Str6ng_P@ssword!'})
+        login_response = self.client.post('/users/login', json={'username': 'test_user', 'password': 'Str6ng_P@ssword!'})
         access_token = json.loads(login_response.data)['access_token']
 
         # Test with a valid token
         headers = {'Authorization': f'Bearer {access_token}'}
-        response = self.client.put('/users', json={'username': 'test_user', 'old_password': 'test_password', 'new_password': 'new_password'}, headers=headers)
+        response = self.client.put('/users', json={'username': 'test_user', 'old_password': 'Str6ng_P@ssword!', 'new_password': 'new_Str6ng_Password'}, headers=headers)
         self.assertEqual(response.status_code, 200)
 
         # Test with an invalid token
         headers = {'Authorization': 'Bearer invalid_token'}
-        response = self.client.put('/users', json={'username': 'test_user', 'old_password': 'test_password', 'new_password': 'new_password'}, headers=headers)
+        response = self.client.put('/users', json={'username': 'test_user', 'old_password': 'Str6ng_P@ssword!', 'new_password': 'new_Str6ng_Password'}, headers=headers)
         self.assertEqual(response.status_code, 401)
 
         # Test with a missing token
-        response = self.client.put('/users', json={'username': 'test_user', 'old_password': 'test_password', 'new_password': 'new_password'})
+        response = self.client.put('/users', json={'username': 'test_user', 'old_password': 'Str6ng_P@ssword!', 'new_password': 'new_Str6ng_Password'})
         self.assertEqual(response.status_code, 401)
 
 if __name__ == '__main__':
